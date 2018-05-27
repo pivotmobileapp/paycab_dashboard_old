b4a1e860144fb87e525d591ac0f68415ea2bccbe
pivotmobileapp <jupiter@pivotmobileapp.com>
2018-05-27 07:16:35 -0400

    jij

--- react/src/services/apiConfig.js
+++ react/src/services/apiConfig.js
@@ -1,6 +1,6 @@
 export default {
 	// local server
-	serverUrl: 'http://oldpaycabapi.herokuapp.com', // use Ip address instead of localhost
+	serverUrl: 'https://oldpaycabapi.herokuapp.com', // use Ip address instead of localhost
 	// port: 3010 // incase of running api-server (node app) in development
 	port: 443 //incase of running api-server (node app) in production
 	// port: 443 //incase of running api-server (node app) on heroku
